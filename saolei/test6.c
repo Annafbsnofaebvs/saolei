a626b5ffba18fda62a98ff273394a0f96fc42837
Annafbsnofaebvs <111216913+Annafbsnofaebvs@users.noreply.github.com>
2023-02-28 18:29:56 +0800

    Add files via upload

---
+++ saolei/test6.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "game6.h"
+void menu()
+{
+	printf("*********************\n");
+	printf("****   1.play   *****\n");
+	printf("****   0.exit   *****\n");
+	printf("*********************\n");
+}
+void game()
+{
+	char mine[ROW][COLS] = {0};//存放布置好的雷的信息
+	char show[ROW][COLS] = {0};//存放排查出的雷的信息
+	//初始化棋盘
+	InitBoard(mine, ROWS, COLS, '0');//'0'
+	InitBoard(show, ROWS, COLS, '*');//'*'
+
+	//打印一下棋盘
+	DisplayBoard(mine, ROW, COL);
+	DisplayBoard(show, ROW, COL);
+
+	//布置雷
+	SetMine(mine, ROW, COL);
+	//DisplayBoard(mine, ROW, COL);
+
+	//排查雷
+	FindMine(mine, show, ROW, COL);
+}
+int main()
+{
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择：>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();//扫雷游戏
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，重新选择\n");
+			break;
+		}		
+	} while (input);
+	return 0;
+}
